88fed1b00f54e94c092dab26834b4c42eccb240a
krzysiu-wilk <xedanion16@gmail.com>
2019-08-02 13:02:17 +0200

    nav v3

--- django-drf-react-quickstart/project/frontend/src/components/Nav.js
+++ django-drf-react-quickstart/project/frontend/src/components/Nav.js
@@ -10,7 +10,7 @@ class CustomNavbar extends React.Component {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link href="http://127.0.0.1:8000/frontend/">Home</Nav.Link>
+              <Nav.Link href="#" className="active">Home</Nav.Link>
               <Nav.Link href="http://127.0.0.1:8000/candidates/">Link</Nav.Link>
               <Nav.Link href="http://127.0.0.1:8000/candidates/skills">Form</Nav.Link>
             </Nav>
